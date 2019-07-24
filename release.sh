6d5a1830a1f80e4cd8343b9f80fdd6813cf39453
Eric Levine <eric@berbix.com>
2019-07-24 15:21:47 -0700

    Fix the release script.

--- release.sh
+++ release.sh
@@ -4,10 +4,10 @@ set -ex
 
 VERSION=$(cat version)
 
-sed -i "" -e "s/const SDK_VERSION = '[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*';/const SDK_VERSION = '$VERSION';/g" projects/berbix/src/lib/berbix-component.ts
+sed -i "" -e "s/const SDK_VERSION = '[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*';/const SDK_VERSION = '$VERSION';/g" projects/berbix/src/lib/berbix.component.ts
 sed -i "" -e "s/  \"version\": \"[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\",/  \"version\": \"$VERSION\",/g" projects/berbix/package.json
 
-git add projects/berbix/src/lib/berbix-component.ts projects/berbix/package.json version
+git add projects/berbix/src/lib/berbix.component.ts projects/berbix/package.json version
 git commit -m "Updating Berbix Angular SDK version to $VERSION"
 git tag -a $VERSION -m "Version $VERSION"
 git push --follow-tags

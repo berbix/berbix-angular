c12968d51d073bf9dd536c7cc915810aefcafc56
Eric Levine <eric@berbix.com>
2019-07-24 15:21:49 -0700

    Updating Berbix Angular SDK version to 0.0.4

--- projects/berbix/src/lib/berbix.component.ts
+++ projects/berbix/src/lib/berbix.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, EventEmitter, Input, Output } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-const SDK_VERSION = '0.0.3';
+const SDK_VERSION = '0.0.4';
 
 export interface FlowCompletedEvent {
   code: string;
